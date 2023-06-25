f1fb056c74f14f4cceb0728b7841d8e59a548d11
Tribhuwan <mathincosmos@gmail.com>
2023-06-25 16:43:54 +0530

    fix env variable

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(compression);
+app.use(compression());
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
